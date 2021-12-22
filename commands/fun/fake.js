873068608a2298a6bd60c984f8a2e3393a0451eb
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-12-22 16:16:24 +0100

    refactor(fake): Refactored

--- commands/fun/fake.js
+++ commands/fun/fake.js
@@ -4,7 +4,8 @@ const Client = require('../../struct/Client');
 module.exports = {
   name: 'fake',
   usage: 'fake [membre] [message]',
-  description: 'Génère un webhook similaire à un membre qui enverra le message spécifié.',
+  description:
+    'Génère un webhook similaire à un membre qui enverra le message spécifié.',
   category: 'Fun',
   img: 'https://image.flaticon.com/icons/png/512/1750/1750186.png',
 
@@ -14,23 +15,27 @@ module.exports = {
    * @param {string[]} args
    */
   async execute(client, message, args) {
+    if (message.channel.type !== 'GUILD_TEXT') return;
     if (!args.length) return client.sendHelpPage(this.name, message);
 
     const member = client.detectMember(message, args[0]);
-    if (!member) return message.inlineReply('<:unchecked:860839603098877953> • Membre spécifié invalide.');
+    if (!member)
+      return message.reply(
+        '<:unchecked:860839603098877953> • Membre spécifié invalide.'
+      );
 
     const name = member.displayName || member.user.username;
-
     const channelWebhooks = await message.channel.fetchWebhooks();
-    let webhook = channelWebhooks.find(w => w.name === name);
 
-    if (!webhook) {
-      webhook = await message.channel.createWebhook(name, {
-        avatar: member.user.displayAvatarURL({ dynamic: true })
-      });
-    }
+    const webhook = channelWebhooks.find((w) => w.name === name);
 
     message.delete();
-    webhook.send(args.slice(1).join(' '));
-  }
+    webhook
+      ? webhook.send(args.slice(1).join(' '))
+      : (
+          await message.channel.createWebhook(name, {
+            avatar: member.user.displayAvatarURL({ dynamic: true }),
+          })
+        ).send(args.slice(1).join(' '));
+  },
 };
