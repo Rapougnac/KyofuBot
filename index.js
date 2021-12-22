1641b38133e8e7cb69c07832b311b77d637b9e38
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-12-22 16:17:02 +0100

    fix: Edited clientOptions

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const { Intents } = require('discord.js');
-require('./struct/Message.js');
-require('./struct/User.js');
-const Client =  require('./struct/Client');
+const Client = require('./struct/Client');
 const client = new Client({
-  ws: {
-    intents: [Intents.ALL]
-  }
+  clientOptions: {
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.DIRECT_MESSAGES,
+    ],
+    partials: ['MESSAGE', 'CHANNEL'],
+  },
 });
 const { connect } = require('mongoose');
 const { dbconnection, token } = require('./util/config.js');
 
 connect(dbconnection, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('[^^] MongoDB connecté !');
-}).catch(error => {
-  client.logError(error);
-});
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('[^^] MongoDB connecté !');
+  })
+  .catch((error) => {
+    client.logError(error);
+  });
 
 client.loadCommands();
 client.loadEvents();
 
-process.on('uncaughtException', error => {
+process.on('uncaughtException', (error) => {
   client.logError(error);
 });
 
-process.on('unhandledRejection', error => {
+process.on('unhandledRejection', (error) => {
   client.logError(error);
 });
 
